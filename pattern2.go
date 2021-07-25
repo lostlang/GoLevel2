4e491db93d9041ee2c7421f4ea2e8e25637540a0
Alec Lang <53492781+lostlang@users.noreply.github.com>
2021-07-25 19:47:20 +0300

    Update builder pattern

--- pattern2.go
+++ pattern2.go
@@ -7,26 +7,30 @@ type Cat struct {
 }
 
 type CatProducer struct {
-	count *int
+	count int
 	name  string
 }
 
-func (prod CatProducer) getCat() *Cat {
-	*prod.count++
-	return &Cat{prod.name + " " + fmt.Sprint(*prod.count)}
+func (prod *CatProducer) getCat() *Cat {
+	prod.count++
+	return &Cat{prod.name + " " + fmt.Sprint(prod.count)}
+}
+
+func (prod *CatProducer) setCount(num int) {
+	prod.count = num
+}
+
+func (prod *CatProducer) setName(s string) {
+	prod.name = s
 }
 
 func main() {
-	var breader1Cats = 0
-	var breader2Cats = 100
-
-	var breader1 = CatProducer{&breader1Cats, "Murzik"}
-	var breader2 = CatProducer{&breader2Cats, "Peach"}
+	var breader1 = CatProducer{}
+	breader1.setCount(0)
+	breader1.setName("Murzik")
 
 	var cat1 = breader1.getCat()
-	var cat2 = breader2.getCat()
-	var cat3 = breader1.getCat()
-	var cat4 = breader1.getCat()
+	var cat2 = breader1.getCat()
 
-	fmt.Println(cat1, cat2, cat3, cat4)
+	fmt.Println(cat1, cat2)
 }
